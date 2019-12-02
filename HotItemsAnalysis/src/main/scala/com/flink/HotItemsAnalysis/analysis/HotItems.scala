415c6dcc48b8ee7d4627b8f19973dd35533f93da
WangLoveWei <2419465931@qq.com>
2019-12-02 19:50:19 +0800

    实现了实时的数量

--- HotItemsAnalysis/src/main/scala/com/flink/HotItemsAnalysis/analysis/HotItems.scala
+++ HotItemsAnalysis/src/main/scala/com/flink/HotItemsAnalysis/analysis/HotItems.scala
@@ -1,14 +1,21 @@
 package com.flink.HotItemsAnalysis.analysis
 
+import java.sql.Timestamp
+
 import com.flink.HotItemsAnalysis.model.{ItemViewCount, UserBehavior}
 import org.apache.flink.api.common.functions.AggregateFunction
+import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
 import org.apache.flink.api.java.tuple.{Tuple, Tuple1}
+import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.scala.function.WindowFunction
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.util.Collector
+
+import scala.collection.mutable.ListBuffer
 
 /**
   * @author 王犇
@@ -40,6 +47,7 @@ object HotItems {
       .timeWindow(Time.seconds(10), Time.seconds(5))
       .aggregate(new CountAgg, new WindowResultFunction)
       .keyBy("windowEnd")
+      .process(new TopNHotItems(3))
       .print()
 
     see.execute()
@@ -62,5 +70,58 @@ object HotItems {
       out.collect(ItemViewCount(itemId, window.getEnd, count))
     }
   }
+
+  class TopNHotItems(topSize: Int) extends KeyedProcessFunction[Tuple, ItemViewCount, String] {
+
+    private var listState: ListState[ItemViewCount] = _
+
+    override def open(parameters: Configuration): Unit = {
+      super.open(parameters)
+      // 命名状态变量的名字和状态变量的类型
+      val itemStateDesc = new ListStateDescriptor[ItemViewCount]("itemState-state", classOf[ItemViewCount])
+      // 定义状态变量
+      listState = getRuntimeContext.getListState(itemStateDesc)
+    }
+
+    override def processElement(value: ItemViewCount, ctx: KeyedProcessFunction[Tuple, ItemViewCount, String]#Context, out: Collector[String]): Unit = {
+      // 每条数据都保存到状态中
+      listState.add(value)
+      // 注册 windowEnd+1 的 EventTime Timer, 当触发时，说明收齐了属于windowEnd窗口的所有商品数据
+      // 也就是当程序看到windowend + 1的水位线watermark时，触发onTimer回调函数
+      ctx.timerService().registerEventTimeTimer(value.windowEnd + 1)
+    }
+
+    override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[Tuple, ItemViewCount, String]#OnTimerContext, out: Collector[String]): Unit = {
+      super.onTimer(timestamp, ctx, out)
+      // 获取收到的所有商品点击量
+      val allItems: ListBuffer[ItemViewCount] = ListBuffer()
+      import scala.collection.JavaConversions._
+
+      for (item <- listState.get()) {
+        allItems += item
+      }
+      listState.clear()
+
+      // 按照点击量从大到小排序
+      val sortedItems = allItems.sortBy(_.count)(Ordering.Long.reverse).take(topSize)
+      // 将排名信息格式化成 String, 便于打印
+      val result: StringBuilder = new StringBuilder
+      result.append("====================================\n")
+      result.append("时间: ").append(new Timestamp(timestamp - 1)).append("\n")
+
+      for (i <- sortedItems.indices) {
+        val currentItem: ItemViewCount = sortedItems(i)
+        // e.g.  No1：  商品ID=12224  浏览量=2413
+        result.append("No").append(i + 1).append(":")
+          .append("  商品ID=").append(currentItem.itemId)
+          .append("  浏览量=").append(currentItem.count).append("\n")
+      }
+      result.append("====================================\n\n")
+      // 控制输出频率，模拟实时滚动结果
+      Thread.sleep(1000)
+      out.collect(result.toString)
+    }
+  }
+
 
 }
